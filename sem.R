e994bc7d5e2e9605c47ab3e75c0b82452f43365f
tadej <tadej.c@gmail.com>
2015-11-29 17:19:49 +0100

    save fix

--- sem.R
+++ sem.R
@@ -3,6 +3,7 @@ library(randomForest)
 library(e1071)
 library(kknn)
 library(nnet)
+library(CORElearn)
 
 mae <- function(observed, predicted)
 {
@@ -38,23 +39,29 @@ coreTree=function(dist,data){
 
 reg.models=c(lm,rpart,coreTree,randomForest,svm)
 reg.m.names=c("lin reg","tree","tree2","forest","svm")
+reg.rmse=0*(1:length(reg.models))
+
+cross.val=5
 
 for(i in 1:length(missing)){
 	data=missing[[i]](raw_data)
-	for(k in 1:10){                     #cross validation
+	for(k in 1:cross.val){                     #cross validation
 		sel <- sample(1:nrow(data), size=as.integer(nrow(data)*0.8), replace=F)
 		learn <- data[sel,]
 		test <- data[-sel,]
 		for(j in 1:length(reg.models)){
 			m=reg.models[[j]](O3_max ~ DATE + TRAJ + SHORT_TRAJ + AMP_TMP2M_mean + AMP_RH_mean + AMP_WS_mean + AMP_PREC_sum ,learn)
 			prediction=predict(m,test)
-			print(reg.m.names[[j]])
-			#print(mae(test$O3_max,prediction))
-			#print(mse(test$O3_max,prediction))
-			#print(rmae(test$O3_max,prediction,mean(learn$O3_max)))
-			print(rmse(test$O3_max,prediction,mean(learn$O3_max)))
-			print('**********************************')
+    	#print(mae(test$O3_max,prediction))
+    	#print(mse(test$O3_max,prediction))
+    	#print(rmae(test$O3_max,prediction,mean(learn$O3_max)))
+			reg.rmse[[j]]=reg.rmse[[j]]+rmse(test$O3_max,prediction,mean(learn$O3_max))
 		}
+	}
+	for(j in 1:length(reg.models)){
+  	print(reg.m.names[[j]])
+  	print(reg.rmse[[j]]/cross.val)
+  	print('**********************************')
 	}
 }
 
