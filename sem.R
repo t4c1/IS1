af999fc376406755b64e2f954035856f87ccd939
tadej <tadej.c@gmail.com>
2015-12-04 22:56:52 +0100

    utežena linearna regresija, optimizirani argumenti

--- sem.R
+++ sem.R
@@ -35,29 +35,51 @@ atributi<-names(raw_data)
 missing=c(na.omit)
 
 coreTree=function(dist,data){
-	CoreModel(dist,data, model="regTree", modelTypeReg = 1)
+	CoreModel(dist,data, model="regTree", modelTypeReg = 6,minNodeWeightTree=3)
 }
 
-reg.models=c(lm,rpart,coreTree,randomForest,svm)
-reg.m.names=c("lin reg","tree","tree2","forest","svm")
+rpartTree=function(dist,data){
+  rpart(dist,data,cp=0.002)
+}
+
+lwReg=function(dist,data){
+  CoreModel(dist,data, model="regTree", modelTypeReg = 8,minNodeWeightTree=100000)
+}
+
+rf=function(dist,data){
+  randomForest(dist,data,ntree=150)
+}
+
+reg.models=c(lm,lwReg,rpartTree,coreTree,randomForest,svm)
+reg.m.names=c("lin reg","localy weighted reg","tree","tree2","forest","svm","avg","weighted avg")
+
+#reg.models=c(rf,rf1,rf2,rf3,rf4,rf5)
+#reg.m.names=c("tree","tree1","tree2","tree3","tree4","tree5","avg","weighted avg")
+
 cla.models<-c("tree", "rf", "knn", "bayes")
 
-cross.val=1
+reg.n.models=length(reg.models)
+
+cross.val=10
 reg.dists=c(O3_max ~ DATE + TRAJ + SHORT_TRAJ + AMP_TMP2M_mean + AMP_RH_mean + AMP_WS_mean + AMP_PREC_sum,
-            PM10   ~ DATE + TRAJ + SHORT_TRAJ + AMP_TMP2M_mean + AMP_RH_mean + AMP_WS_mean + AMP_PREC_sum,
-            PM2.5  ~ DATE + TRAJ +              AMP_TMP2M_mean + AMP_RH_mean + AMP_WS_mean + AMP_PREC_sum,
+            #PM10   ~ DATE + TRAJ + SHORT_TRAJ + AMP_TMP2M_mean + AMP_RH_mean + AMP_WS_mean + AMP_PREC_sum,
+            #PM2.5  ~ DATE + TRAJ +              AMP_TMP2M_mean + AMP_RH_mean + AMP_WS_mean + AMP_PREC_sum,
             PM2.5  ~ DATE + TRAJ + SHORT_TRAJ + AMP_TMP2M_mean + AMP_RH_mean + AMP_WS_mean + AMP_PREC_sum)
 
-reg.test.idx=c("O3_max","PM10","PM2.5")
+reg.test.idx=c("O3_max","PM2.5")
+reg.n.comb=2
+
+reg.weights=1/c(0.37,0.62,0.43,0.36,0.28,0.32)**15
+reg.weights=reg.weights/sum(reg.weights)
 
 #za hitrejse izvajanje
-class<-T
-regre<-F
+class<-F
+regre<-T
 for(i in 1:length(missing)){
 	data=missing[[i]](raw_data)
 	if(regre){
   	for(l in 1:length(reg.dists)){
-      reg.rmse=0*(1:length(reg.models)+1)
+      reg.rmse=0*(1:(reg.n.models+reg.n.comb))
       reg.attrs=c(0)
       for(k in 1:cross.val){                     #cross validation
       	sel <- sample(1:nrow(data), size=as.integer(nrow(data)*0.8), replace=F)
@@ -65,7 +87,7 @@ for(i in 1:length(missing)){
       	test <- data[-sel,]
       	predictions=data.frame()
     	  reg.attrs=reg.attrs+attrEval(reg.dists[[l]], learn, "RReliefFsqrDistance")
-    		for(j in 1:length(reg.models)){
+    		for(j in 1:reg.n.models){
     			m=reg.models[[j]](reg.dists[[l]] ,learn)
     			prediction=predict(m,test)
     			predictions=rbind(predictions,prediction)
@@ -75,14 +97,21 @@ for(i in 1:length(missing)){
     			reg.rmse[[j]]=reg.rmse[[j]]+rmse(test[,reg.test.idx[l]],prediction,mean(learn[,reg.test.idx[l]]))
     		}
     	  #kombinirani modeli
-    	  prediction=rowSums(predictions)
+    	  prediction=colSums(predictions)/reg.n.models
+    	  j=j+1
+    	  reg.rmse[[j]]=reg.rmse[[j]]+rmse(test[,reg.test.idx[l]],prediction,mean(learn[,reg.test.idx[l]]))
+    	  
+    	  for(k in 1:reg.n.models){
+    	    predictions[k,]=predictions[k,]*reg.weights[k]
+    	  }
+    	  prediction=colSums(predictions)
     	  j=j+1
     	  reg.rmse[[j]]=reg.rmse[[j]]+rmse(test[,reg.test.idx[l]],prediction,mean(learn[,reg.test.idx[l]]))
   		}
   	  print("*********************************")
   	  print(reg.dists[[l]])
   	  print(reg.attrs)
-    	for(j in 1:length(reg.models)){
+    	for(j in 1:(reg.n.models+reg.n.comb)){
     	  print(reg.m.names[[j]])
     	  print(reg.rmse[[j]]/cross.val)
     	}
