b6d1a1d8f288983bc7874da470bd83114f2fb6ce
tadej <tadej.c@gmail.com>
2015-12-06 22:45:33 +0100

    na->avg

--- sem.R
+++ sem.R
@@ -8,22 +8,22 @@ library(ipred)
 #setwd("F:/Users/Tadej/Documents/Fax dn/Is/IS1")
 mae <- function(observed, predicted)
 {
-	mean(abs(observed - predicted))
+  mean(abs(observed - predicted))
 }
 
 rmae <- function(observed, predicted, mean.val) 
 {  
-	sum(abs(observed - predicted)) / sum(abs(observed - mean.val))
+  sum(abs(observed - predicted)) / sum(abs(observed - mean.val))
 }
 
 mse <- function(observed, predicted)
 {
-	mean((observed - predicted)^2)
+  mean((observed - predicted)^2)
 }
 
 rmse <- function(observed, predicted, mean.val) 
 {  
-	sum((observed - predicted)^2)/sum((observed - mean.val)^2)
+  sum((observed - predicted)^2)/sum((observed - mean.val)^2)
 }
 
 getSeason <- function(DATES) {
@@ -89,8 +89,8 @@ removeNARows<-function(final,col){
 }
 
 klasifikacija<-function(data=data,indexObserved=indexObserved,index=index,odstotek=odstotek,cross=cross){
-  res1<-c(0,0,0,0,0);
-  res2<-c(0,0,0,0);
+  res1<-c(0,0,0,0,0,0,0);
+  res2<-c(0,0,0,0,0,0);
   for(k in 1:cross){
     sel <- sample(1:nrow(data), size=as.integer(nrow(data)*odstotek), replace=F)
     learn <- data[sel,]
@@ -118,7 +118,7 @@ klasifikacija<-function(data=data,indexObserved=indexObserved,index=index,odstot
     res1<-as.vector(res1)+as.vector(ca.vals)
     res2<-as.vector(res2)+as.vector(cv.res)
   }
-  x<-sprintf("%7s",c("major.", "tree", "rf", "knn", "bayes"))
+  x<-sprintf("%7s",c("major.","rf","rfNear","tree","knn","knnKernel","bayes"))
   print(x)
   print(res1/cross)
   x<-sprintf("%7s",cla.models)
@@ -127,7 +127,21 @@ klasifikacija<-function(data=data,indexObserved=indexObserved,index=index,odstot
   
 }
 
+replaceWithAverage<-function(DATA){
+  rez<-DATA;
+  for(i in 1:length(DATA)){
+    avg<-mean(as.numeric(raw_data[[i]]), na.rm=TRUE)
+    for(j in 1:length(rez[[i]])){
+      if(is.na(rez[[j,i]])){
+        rez[[j,i]]<-avg
+      }
+    }
+  }
+  return (rez)
+}
+
 raw_data <- read.table("pollution.txt", header = T, sep=",")
+raw_data=replaceWithAverage(raw_data)
 raw_data$TRAJ=as.factor(raw_data$TRAJ)
 raw_data$SHORT_TRAJ=as.factor(raw_data$SHORT_TRAJ)
 season<-dates2Season(dates=raw_data$DATE)
@@ -145,7 +159,7 @@ atributi<-names(raw_data)
 missing=c(na.omit)
 
 coreTree=function(dist,data){
-	CoreModel(dist,data, model="regTree", modelTypeReg = 6,minNodeWeightTree=3)
+  CoreModel(dist,data, model="regTree", modelTypeReg = 6,minNodeWeightTree=3)
 }
 
 rpartTree=function(dist,data){
@@ -166,7 +180,7 @@ reg.m.names=c("lin reg","localy weighted reg","tree","tree2","forest","svm","avg
 #reg.models=c(rf,rf1,rf2,rf3,rf4,rf5)
 #reg.m.names=c("tree","tree1","tree2","tree3","tree4","tree5","avg","weighted avg")
 
-cla.models<-c("tree", "rf", "knn", "bayes")
+cla.models<-c("rf","rfNear","tree","knn","knnKernel","bayes")
 
 reg.n.models=length(reg.models)
 
@@ -184,68 +198,70 @@ reg.weights=1/c(0.37,0.62,0.43,0.36,0.28,0.32)**15
 reg.weights=reg.weights/sum(reg.weights)
 
 #za hitrejse izvajanje
-class<-T
+class<-F
 regre<-T
 for(i in 1:length(missing)){
-	data=missing[[i]](raw_data)
-	print(length(data[[1]]))
-	if(regre){
-  	for(l in 1:length(reg.dists)){
+  data=missing[[i]](raw_data)
+  print(length(data[[1]]))
+  if(regre){
+    for(l in 1:length(reg.dists)){
       reg.rmse=0*(1:(reg.n.models+reg.n.comb))
       reg.attrs=c(0)
       for(k in 1:cross.val){                     #cross validation
-      	sel <- sample(1:nrow(data), size=as.integer(nrow(data)*0.8), replace=F)
-      	learn <- data[sel,]
-      	test <- data[-sel,]
-      	predictions=data.frame()
-    	  reg.attrs=reg.attrs+attrEval(reg.dists[[l]], learn, "RReliefFsqrDistance")
-    		for(j in 1:reg.n.models){
-    			m=reg.models[[j]](reg.dists[[l]] ,learn)
-    			prediction=predict(m,test)
-    			predictions=rbind(predictions,prediction)
-        	#print(mae(test$O3_max,prediction))
-        	#print(mse(test$O3_max,prediction))
-        	#print(rmae(test$O3_max,prediction,mean(learn$O3_max)))
-    			reg.rmse[[j]]=reg.rmse[[j]]+rmse(test[,reg.test.idx[l]],prediction,mean(learn[,reg.test.idx[l]]))
-    		}
-    	  #kombinirani modeli
-    	  prediction=colSums(predictions)/reg.n.models
-    	  j=j+1
-    	  reg.rmse[[j]]=reg.rmse[[j]]+rmse(test[,reg.test.idx[l]],prediction,mean(learn[,reg.test.idx[l]]))
-    	  
-    	  for(k in 1:reg.n.models){
-    	    predictions[k,]=predictions[k,]*reg.weights[k]
-    	  }
-    	  prediction=colSums(predictions)
-    	  j=j+1
-    	  reg.rmse[[j]]=reg.rmse[[j]]+rmse(test[,reg.test.idx[l]],prediction,mean(learn[,reg.test.idx[l]]))
-  		}
-  	  print("*********************************")
-  	  print(reg.dists[[l]])
-  	  print(reg.attrs)
-    	for(j in 1:(reg.n.models+reg.n.comb)){
-    	  print(reg.m.names[[j]])
-    	  print(reg.rmse[[j]]/cross.val)
-    	}
-  	}
-	}
-	#klasifikacija
-	if(class){
-	  data<-removeNARows(raw_data,c(1:7))
-	  data[,11]<-cut(data[,11], c(-Inf, 1, 2, 3, Inf), labels=c("SPRING", "SUMMER", "FALL","WINTER"))
-	  data1<-data[complete.cases(data[,8]),]
-	  
-		data1[,8]<-cut(data1[,8], c(-Inf, 60, 120, 180, Inf), labels=c("LOW", "MEDIUM", "HIGH", "EXTREME"))
-		
-		print("O3_max")
-		klasifikacija(data=data1,indexObserved =8 ,index=1,odstotek=0.8,cross=10)
-	 
-		print("PM10")
-		data2<-data[complete.cases(data[,9]),]
-		data2[,9]<-cut(data2[,9], c(-Inf, 35, 50, Inf), labels=c("LOW", "MODERATE", "HIGH"))
-		#klasifikacija(data=data2,indexObserved =9 ,index=2,odstotek=0.8,cross=10)
-	}
+        sel <- sample(1:nrow(data), size=as.integer(nrow(data)*0.8), replace=F)
+        learn <- data[sel,]
+        test <- data[-sel,]
+        predictions=data.frame()
+        reg.attrs=reg.attrs+attrEval(reg.dists[[l]], learn, "RReliefFsqrDistance")
+        for(j in 1:reg.n.models){
+          m=reg.models[[j]](reg.dists[[l]] ,learn)
+          prediction=predict(m,test)
+          predictions=rbind(predictions,prediction)
+          #print(mae(test$O3_max,prediction))
+          #print(mse(test$O3_max,prediction))
+          #print(rmae(test$O3_max,prediction,mean(learn$O3_max)))
+          reg.rmse[[j]]=reg.rmse[[j]]+rmse(test[,reg.test.idx[l]],prediction,mean(learn[,reg.test.idx[l]]))
+        }
+        #kombinirani modeli
+        prediction=colSums(predictions)/reg.n.models
+        j=j+1
+        reg.rmse[[j]]=reg.rmse[[j]]+rmse(test[,reg.test.idx[l]],prediction,mean(learn[,reg.test.idx[l]]))
+        
+        for(k in 1:reg.n.models){
+          predictions[k,]=predictions[k,]*reg.weights[k]
+        }
+        prediction=colSums(predictions)
+        j=j+1
+        reg.rmse[[j]]=reg.rmse[[j]]+rmse(test[,reg.test.idx[l]],prediction,mean(learn[,reg.test.idx[l]]))
+      }
+      print("*********************************")
+      print(reg.dists[[l]])
+      print(reg.attrs)
+      for(j in 1:(reg.n.models+reg.n.comb)){
+        print(reg.m.names[[j]])
+        print(reg.rmse[[j]]/cross.val)
+      }
+    }
+  }
+  #klasifikacija
+  if(class){
+    data<-removeNARows(raw_data,c(1:7))
+    data[,11]<-cut(data[,11], c(-Inf, 1, 2, 3, Inf), labels=c("SPRING", "SUMMER", "FALL","WINTER"))
+    data1<-data[complete.cases(data[,8]),]
+    
+    data1[,8]<-cut(data1[,8], c(-Inf, 60, 120, 180, Inf), labels=c("LOW", "MEDIUM", "HIGH", "EXTREME"))
+    
+    print("O3_max")
+    klasifikacija(data=data1,indexObserved =8 ,index=1,odstotek=0.8,cross=30)
+    
+    print("PM10")
+    data2<-data[complete.cases(data[,9]),]
+    data2[,9]<-cut(data2[,9], c(-Inf, 35, 50, Inf), labels=c("LOW", "MODERATE", "HIGH"))
+    klasifikacija(data=data2,indexObserved =9 ,index=2,odstotek=0.8,cross=30)
+  }
 }
+
+#"rf","rfNear","tree","knn","knnKernel","bayes"
 
 
 
